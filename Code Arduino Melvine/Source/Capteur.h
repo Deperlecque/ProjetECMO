8ec8fcb72cdce4265ae4263e357e8daa2501afd9
Deperlecque <melvine.deperlecquepro@gmail.com>
2025-03-25 14:21:19 +0100

    Add files via upload

---
+++ Code Arduino Melvine/Source/Capteur.h
@@ -0,0 +1,25 @@
+#ifndef CAPTEUR_H
+#define CAPTEUR_H
+
+#include "HX711.h"
+#include "Arduino.h"
+
+class CCapteur {
+private:
+    HX711 hx711;
+    int dtPin;
+    int sckPin;
+    float calibrationFactor;
+
+public:
+    CCapteur(int dtPin, int sckPin);
+    ~CCapteur();
+    void setupCapteur();
+    long getPression(); 
+    long getRythme(); 
+    void tareSensor();
+    void setCalibrationFactor(float factor);
+    float getCalibrationFactor();
+};
+
+#endif
