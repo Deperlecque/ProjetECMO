496ea8088f1452796c57cd90f1ff41e356c61141
alex <51286777+Vapeurfr62@users.noreply.github.com>
2025-03-26 08:44:31 +0100

    - Ajouts de commentaires - Optimisation des calculs pour les données test

--- Site Internet/script.js
+++ Site Internet/script.js
@@ -1,4 +1,4 @@
-let testValue = 125
+let testValue = 125 // initialisation d'une valeur de test
 function myYRangeFunction(range) {
     // TODO implement your calculation using range.min and range.max
     var min = 0;
@@ -13,15 +13,15 @@ function myYRangeFunction(range) {
       series = new TimeSeries();
   
       setInterval(function() {
+        // Utilisation d'une valeur de test pour afficher sur le graphique
+        // une fois sur deux, le graphique monte à 125 puis redescend
         series.append(Date.now(), testValue);
         if(Math.random() > 0.5 && testValue == 125)
             { 
-        testValue += 125;
-        if(testValue > 250)
-            testValue = 125;
+        testValue = 250;
+        testValue = 125;
         } else 
         testValue = 125;
-
       }, 1000);
       
   chart.addTimeSeries(series, {lineWidth:1.7,strokeStyle:'#00ff00',interpolation:'bezier'});
