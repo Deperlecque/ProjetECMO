c88c7d12db71c989dd6f2a9729cd9243f8fbcf63
alex <51286777+Vapeurfr62@users.noreply.github.com>
2025-03-28 14:42:50 +0100

    Changement des valeurs de test

--- Site Internet/script.js
+++ Site Internet/script.js
@@ -1,4 +1,4 @@
-let testValue = 125 // initialisation d'une valeur de test
+let testValue = -125 // initialisation d'une valeur de test
 function myYRangeFunction(range) {
     // TODO implement your calculation using range.min and range.max
     var min = 0;
@@ -16,12 +16,12 @@ function myYRangeFunction(range) {
         // Utilisation d'une valeur de test pour afficher sur le graphique
         // une fois sur deux, le graphique monte à 125 puis redescend
         series.append(Date.now(), testValue);
-        if(Math.random() > 0.5 && testValue == 125)
+        if(Math.random() > 0.5 && testValue == -125)
             { 
-        testValue = 250;
-        testValue = 125;
+        testValue = -250;
+        testValue = -125;
         } else 
-        testValue = 125;
+        testValue = -125;
       }, 1000);
       
   chart.addTimeSeries(series, {lineWidth:1.7,strokeStyle:'#00ff00',interpolation:'bezier'});
